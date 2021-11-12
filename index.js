d26458b9c6d5fc0acbf67bea7ae5bfc52e524cf2
José Ángel <joseyanez2298@gmail.com>
2021-11-11 21:37:35 -0500

    agregado CRUD docente básico

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const {dbConnection}=require('./database/config');
 
 
 
+
 //Crear el servidor de express
 const app=express();
 
 //Configurar cors
 app.use(cors())
-
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 //Conexion a la base de datos
 dbConnection();
 
 
+const routes_docente = require('./routes/docente');
+
+app.use('/api', routes_docente);
 
 
 
