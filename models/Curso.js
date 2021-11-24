affc8ec24a2f596c15936430c76f5674aeec4720
aldacco <a1dac0.02@hotmail.com>
2021-11-23 21:27:52 -0500

    nuevo atributo en el modelo cursos

--- models/Curso.js
+++ models/Curso.js
@@ -9,6 +9,10 @@ const Cursos = new Schema({
         type: String,
         required: true
     },
+    tipo: {
+        type: String,
+        required: true
+    },
     categoria:{
         type: String,
         required: true
