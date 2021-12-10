5f6da2712b5221d6285d3e19ddc02c96a4adee6a
aldacco <a1dac0.02@hotmail.com>
2021-12-09 21:24:39 -0500

    cursos por docente

--- controllers/matricula.js
+++ controllers/matricula.js
@@ -1,17 +1,17 @@
 const { response } = require('express')
-const Matricula=require('../models/matricula')
+const Matricula = require('../models/matricula')
 
 
 
 
-const getMatriculas=async (req,res)=>{
+const getMatriculas = async (req, res) => {
 
 
-    const matriculas=await Matricula.find()
-        .populate('usuario','nombre apellido')
+    const matriculas = await Matricula.find()
+        .populate('usuario', 'nombre apellido')
         .populate('curso')
     res.json({
-        ok:true,
+        ok: true,
         matriculas
     })
 
@@ -31,115 +31,123 @@ const getMatriculas=async (req,res)=>{
 //         date:Date,
 //         flag:Boolean      
 //     }],
-    
+
 // }],
 // curso:{
 //     type:Schema.Types.ObjectId,
 //     ref:'Curso',
 //     required:true
-    
+
 // }
-const crearMatricula= async (req,res=response)=>{
-    const uid=req.uid
-    const matricula=new Matricula({
-        user:uid,
+const crearMatricula = async (req, res = response) => {
+    const uid = req.uid
+    const matricula = new Matricula({
+        user: uid,
         ...req.body
     })
 
-   
+
     try {
-        console.log('estos es'+uid)
+        console.log('estos es' + uid)
         // const matricula= new Matricula(req.body);
         // matricula.docente[0].asistencias[0].date instanceof Date;
         // matricula.alumno[0].asistencias[0].date instanceof Date;
-        const matriculaDB=await matricula.save()
+        const matriculaDB = await matricula.save()
 
 
         // console.log(docente[0].codDocente)
 
         console.log(req.body)
         res.json({
-            ok:true,
-            matricula:matriculaDB
+            ok: true,
+            matricula: matriculaDB
         })
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            ok:false,
-            msg:"Error inesperado"
+            ok: false,
+            msg: "Error inesperado"
         })
     }
-    
+
 
 }
-const actualizarMatricula= async (req,res=response)=>{
-    const uid=req.uid
-    const id=req.params.id;
+const actualizarMatricula = async (req, res = response) => {
+    const uid = req.uid
+    const id = req.params.id;
     try {
-        const matriculaDB= await Matricula.findById(id);
-        if(!matriculaDB){
+        const matriculaDB = await Matricula.findById(id);
+        if (!matriculaDB) {
             return res.status(404).json({
-                ok:false,
-                msg:"No existe la matricula con ese id"
+                ok: false,
+                msg: "No existe la matricula con ese id"
             })
         }
 
         // update
-        const cambiosMatricula={
+        const cambiosMatricula = {
             ...req.body,
-            user:uid
+            user: uid
         }
-        const matriculaActualizada= await Matricula.findByIdAndUpdate(id,cambiosMatricula,{new:true})
+        const matriculaActualizada = await Matricula.findByIdAndUpdate(id, cambiosMatricula, { new: true })
         res.json({
-            ok:true,
-            msg:'Matricula Actualizada',
-            matricula:matriculaActualizada
+            ok: true,
+            msg: 'Matricula Actualizada',
+            matricula: matriculaActualizada
         })
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            ok:false,
-            msg:"Error inesperado"
+            ok: false,
+            msg: "Error inesperado"
         })
 
     }
 
 }
-const borrarMatricula= async (req,res=response)=>{
-    const id=req.params.id;
+const borrarMatricula = async (req, res = response) => {
+    const id = req.params.id;
     try {
-        const matriculaDB= await Matricula.findById(id);
-        if(!matriculaDB){
+        const matriculaDB = await Matricula.findById(id);
+        if (!matriculaDB) {
             return res.status(404).json({
-                ok:false,
-                msg:"No existe la matricula con ese id"
+                ok: false,
+                msg: "No existe la matricula con ese id"
             })
         }
 
         // update
-        
+
         await Matricula.findByIdAndDelete(id)
         res.json({
-            ok:true,
-            msg:"Matricula eliminada",
+            ok: true,
+            msg: "Matricula eliminada",
         })
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            ok:false,
-            msg:"Error inesperado"
+            ok: false,
+            msg: "Error inesperado"
         })
-        
+
     }
 
 }
 
+const buscarMisCursos = async (req, res) => {
+    const id = req.params.id
+    const matricula = await Matricula.find({ usuario: [{ _id: id }] })
+        .populate('usuario', 'nombre apellido')
+        .populate('curso')
+    res.json(matricula)
 
+}
+//{docente: [{codDocente: codigo}]}
 
-
-module.exports={
+module.exports = {
     getMatriculas,
     crearMatricula,
     actualizarMatricula,
-    borrarMatricula
+    borrarMatricula,
+    buscarMisCursos
 }
